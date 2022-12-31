c5f8e0d140d5ec16091affa87efbbf4b2be12f56
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2022-12-31 18:00:08 +1300

    ✨ Add banner component to top page

---
+++ pages/index.jsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Flex, Box, Text, Button } from '@chakra-ui/react';
+import { Banner } from './Banner';
+
+const Home = () => {
+  return (
+    <Box>
+      <Banner
+        purpose='RENT A HOME'
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
+        title1='Rental Homes for'
+        title2='Everyone'
+        desc1='Explore Apartments, Villas, Homes'
+        desc2='and More'
+        linkName='/search?purpose=for-rent'
+        buttonText='Explore Renting'
+      />
+      <Flex flexWrap='wrap'>
+        {/* Fetch the properties and map over them... */}
+      </Flex>
+      <Banner
+        purpose='BUY A HOME'
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
+        title1='Find, Buy & Own Your'
+        title2='Dream Home'
+        desc1='Explore Apartments, Villas, Homes'
+        desc2='and More'
+        linkName='/search?purpose=for-sale'
+        buttonText='Explore Buying'
+      />
+      <Flex flexWrap='wrap'>
+        {/* Fetch the properties and map over them... */}
+      </Flex>
+    </Box>
+  );
+};
+
+export default Home;

e0e1a96dc6e29806e619a99c3b29762a5408065c
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-01-02 13:22:16 +1300

    ✨ Add search page

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import {
+  Flex,
+  Box,
+  Select,
+  Text,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { MdCancel } from 'react-icons/md';
+import Image from 'next/image';
+
+export const SearchFilters = () => {
+  return (
+    <Flex>
+      <h1>SearchFilters</h1>
+    </Flex>
+  );
+};
